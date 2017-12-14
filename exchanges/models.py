f7e68c70e87ea92de4d26257325bd688d52975bc
harzo <harzojunior@gmail.com>
2017-12-14 17:25:43 +0100

    Created Currency, Exchange, ExchangePair models Added new app to settings

--- exchanges/models.py
+++ exchanges/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=10)
+    sign = models.CharField(max_length=10)
+    sign_after = models.BooleanField(default=True)
+    crypto = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name+' ('+self.code+')'
+
+
+class Exchange(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class ExchangePair(models.Model):
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE, related_name='pairs')
+    left = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='left', null=False)
+    right = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='right', null=False)
+
+    def __str__(self):
+        return self.exchange.name+': '+self.left.code+'/'+self.right.code
